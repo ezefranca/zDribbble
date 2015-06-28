675103d05dcff483ec25a31e8fa96c6bc8925374
Ezequiel França <ezequiel.ifsp@gmail.com>
2015-06-28 16:15:55 -0300

    Base project

---
+++ zDribbble/zDribbble/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zDribbble
+//
+//  Created by Ezequiel França on 6/28/15.
+//  Copyright (c) 2015 Ezequiel França. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zDribbble/zDribbbleTests/zDribbbleTests.m
@@ -0,0 +1,40 @@
+//
+//  zDribbbleTests.m
+//  zDribbbleTests
+//
+//  Created by Ezequiel França on 6/28/15.
+//  Copyright (c) 2015 Ezequiel França. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface zDribbbleTests : XCTestCase
+
+@end
+
+@implementation zDribbbleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
